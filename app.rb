15185fed7be8217e20de858d4bf5af634524bf0d
Aileen Cacayorin & Austin Kincaid <student@epicodus.com>
2015-08-19 15:44:16 -0700

    It will add patrons to a patron list page

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require("./lib/author")
 require("./lib/patron")
 require("./lib/book")
 require('pg')
+require('pry')
 
 DB = PG.connect({:dbname => "library"})
 
@@ -13,12 +14,15 @@ get('/') do
 end
 
 get('/books') do
+  @all_books = Book.all
   erb(:book_list)
 end
 
 post('/books') do
   new_book = Book.new(:id => nil, :title => params.fetch('new_book'))
   new_book.save
+  @all_books = Book.all
+
   erb(:book_list)
 end
 
@@ -32,4 +36,16 @@ delete('/books/:id') do
   @book = Book.find(params.fetch('id').to_i)
   @book.delete()
   erb(:book_list)
+end
+
+get('/patrons') do
+  @all_patrons = Patron.all
+  erb(:patron_list)
+end
+
+post('/patrons') do
+  new_patron = Patron.new(:id => nil, :name => params.fetch('patron_name'))
+  new_patron.save
+  @all_patrons = Patron.all
+  erb(:patron_list)
 end
